adcc38036070195d8c024e4af2d1c58ce8ff4c30
Ralph Avalon <amoedo.raphael@gmail.com>
2018-04-09 08:14:48 -0300

    Add ListBooks page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Bookshelf from './components/Bookshelf'
+import ListBooks from './components/pages/ListBooks'
 
 class BooksApp extends React.Component {
   state = {
@@ -42,21 +42,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf title="Currently Reading" books={books.filter((book) => book.shelf === 'currentlyReading')} />
-                <Bookshelf title="Want to Read" books={books.filter((book) => book.shelf === 'wantToRead')} />
-                <Bookshelf title="Read" books={books.filter((book) => book.shelf === 'read')} />
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
+          <ListBooks books={books} onAddBook={() => this.setState({ showSearchPage: true })} />
         )}
       </div>
     )
---
+++ src/components/pages/ListBooks.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Bookshelf from '../Bookshelf'
+
+class ListBooks extends React.Component {
+
+    render() {
+        const { books, onAddBook } = this.props;
+        return (
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <Bookshelf title="Currently Reading" books={books.filter((book) => book.shelf === 'currentlyReading')} />
+                <Bookshelf title="Want to Read" books={books.filter((book) => book.shelf === 'wantToRead')} />
+                <Bookshelf title="Read" books={books.filter((book) => book.shelf === 'read')} />
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={onAddBook}>Add a book</a>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default ListBooks
